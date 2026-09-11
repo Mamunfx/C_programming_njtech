23b09d5158f67aabe91f870764d7c3c5d22d42ab
Mamunfx <md01993049420@gmail.com>
2026-09-11 10:35:45 +0800

    comment deleted

--- Assignment_3/Code1.c
+++ Assignment_3/Code1.c
@@ -4,13 +4,3 @@ int main() {
     printf("This is my first program.");
     return 0;
 }
-
-/**
- * echo "# C_programming_njtech" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Mamunfx/C_programming_njtech.git
-git push -u origin main
-**/
